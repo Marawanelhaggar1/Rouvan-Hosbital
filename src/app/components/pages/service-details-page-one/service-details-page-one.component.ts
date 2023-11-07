e2597f417998f330a98c256c5ff849ee89483f0b
Marawanelhaggar1 <marawanmagdy91@gmail.com>
2023-11-07 22:22:35 +0200

    service details edit 25

--- src/app/components/pages/service-details-page-one/service-details-page-one.component.ts
+++ src/app/components/pages/service-details-page-one/service-details-page-one.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-service-details-page-one',
-  templateUrl: './service-details-page-one.component.html',
-  styleUrls: ['./service-details-page-one.component.scss']
+    selector: 'app-service-details-page-one',
+    templateUrl: './service-details-page-one.component.html',
+    styleUrls: ['./service-details-page-one.component.scss'],
 })
 export class ServiceDetailsPageOneComponent implements OnInit {
+    lang?: string;
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        if (localStorage.getItem('lang')) {
+            this.lang = JSON.parse(localStorage.getItem('lang')!);
+        } else {
+            this.lang = 'ltr';
+        }
+    }
 }
